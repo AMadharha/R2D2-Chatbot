c5d85837e5a60d7b09668682e96a394c91ec82d0
Ankush Madharha <ankushmadharha@gmail.com>
2022-12-16 16:58:26 -0500

    renamed text to prompt

--- chat_gbt.py
+++ chat_gbt.py
@@ -1,8 +1,9 @@
 from pyChatGPT import ChatGPT
 from secret import SESSION_TOKEN
 
-def get_response(text):
+# Function to get 
+def get_response(prompt):
     api = ChatGPT(SESSION_TOKEN)
-    response = api.send_message(text + " (in 1-3 sentences)")
+    response = api.send_message(prompt + " (in 1-3 sentences)")
     api.close()
     return response["message"]
